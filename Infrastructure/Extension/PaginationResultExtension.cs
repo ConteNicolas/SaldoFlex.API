acaedac54db7be0e695609c6a195805e5cd8a91a
Nicolas Conte <contenicolascv@gmail.com>
2025-11-08 06:49:42 -0300

    feat(extension): Add linq extension related to pagination

---
+++ Infrastructure/Extension/PaginationResultExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaldoFlex.API.Shared.Models;
+
+namespace SaldoFlex.API.Infrastructure.Extension;
+
+public static class PaginationResultExtension
+{
+    public static Task<PaginatedResult<TDestination>> ToPaginatedResultAsync<TDestination>(
+        this IQueryable<TDestination> queryable, int currentPage, int pageSize, CancellationToken cancellationToken)
+        where TDestination : class
+    {
+        return PaginatedResult<TDestination>.CreateAsync(queryable.AsNoTracking(), currentPage, pageSize, cancellationToken);
+    }
+}
