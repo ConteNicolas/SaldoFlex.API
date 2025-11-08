f98c4916e906b98ec0ef1efd4821e54673c12984
Nicolas Conte <contenicolascv@gmail.com>
2025-11-08 05:34:16 -0300

    feat(domain): Add user entity

---
+++ Domain/User.cs
@@ -0,0 +1,11 @@
+﻿using SaldoFlex.API.Domain.Abstractions;
+
+namespace SaldoFlex.API.Domain;
+
+public class User : BaseEntity
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string? Email { get; set; }
+    public DateTime? LastLogin { get; set; }
+}
