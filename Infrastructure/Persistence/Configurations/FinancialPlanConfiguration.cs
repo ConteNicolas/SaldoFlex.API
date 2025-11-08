d93e159e235900e5b412681d3709955d9b59664d
Nicolas Conte <contenicolascv@gmail.com>
2025-11-08 05:34:37 -0300

    feat(ef-configuration): Add financial plan and scene one to one configuration

---
+++ Infrastructure/Persistence/Configurations/FinancialPlanConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaldoFlex.API.Domain;
+
+namespace SaldoFlex.API.Infrastructure.Persistence.Configurations;
+
+public class FinancialPlanConfiguration : IEntityTypeConfiguration<FinancialPlan>
+{
+    public void Configure(EntityTypeBuilder<FinancialPlan> builder)
+    {
+        builder
+            .HasOne(x => x.FinancialScene)
+            .WithOne(b => b.FinancialPlan)
+            .HasForeignKey<FinancialPlan>(b => b.FinancialSceneId);
+    }
+}
---
+++ Infrastructure/Persistence/Configurations/FinancialSceneConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaldoFlex.API.Domain;
+
+namespace SaldoFlex.API.Infrastructure.Persistence.Configurations;
+
+public class FinancialSceneConfiguration : IEntityTypeConfiguration<FinancialScene>
+{
+    public void Configure(EntityTypeBuilder<FinancialScene> builder)
+    {
+        builder
+            .HasOne(x => x.FinancialPlan)
+            .WithOne(b => b.FinancialScene)
+            .HasForeignKey<FinancialScene>(b => b.FinancialPlanId);
+    }
+}
