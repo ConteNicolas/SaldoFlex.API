5cd2f7c1b900db5e6ce7507b5a72563a2fc133d5
Nicolas Conte <contenicolascv@gmail.com>
2025-11-16 05:05:00 -0300

    feat(domain): This will allow us to have multiple incomes with diferent amounts and source

---
+++ Domain/FinancialSceneIncome.cs
@@ -0,0 +1,9 @@
+﻿using SaldoFlex.API.Domain.Abstractions;
+
+namespace SaldoFlex.API.Domain;
+
+public class FinancialSceneIncome : BaseEntity
+{
+    public string Name { get; set; }
+    public double Amount { get; set; }
+}
