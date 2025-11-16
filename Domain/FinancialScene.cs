cc45bfa9359113049dbbf59e3130c6baea86749c
Nicolas Conte <contenicolascv@gmail.com>
2025-11-16 05:02:31 -0300

    feat(schema): now we can setup all the incomes we want into the scene

--- Domain/FinancialScene.cs
+++ Domain/FinancialScene.cs
@@ -4,10 +4,9 @@ namespace SaldoFlex.API.Domain;
 
 public class FinancialScene : BaseEntity
 {
-    public string? Description { get; set; }
-
     public Guid FinancialPlanId { get; set; }
     public virtual FinancialPlan FinancialPlan { get; set; }
 
     public virtual ICollection<FinancialSceneGroup> Groups { get; set; } = new List<FinancialSceneGroup>();
+    public virtual ICollection<FinancialSceneIncome> Incomes { get; set; } = new List<FinancialSceneIncome>();
 }
