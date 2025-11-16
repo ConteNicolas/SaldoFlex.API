912f97786bf4c3adf92fd13f4b9bbcf99fedc4ac
Nicolas Conte <contenicolascv@gmail.com>
2025-11-16 05:02:04 -0300

    feat(schema): add optional description field.

--- Domain/FinancialPlan.cs
+++ Domain/FinancialPlan.cs
@@ -5,6 +5,7 @@ namespace SaldoFlex.API.Domain;
 public class FinancialPlan : BaseEntity
 {
     public string Name { get; set; }
+    public string? Description { get; set; }
 
     public Guid FinancialSceneId { get; set; }
     public virtual FinancialScene FinancialScene { get; set; }
