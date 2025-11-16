140a30a583f503bb3e2646b97dcb3f3697fbcf02
Nicolas Conte <contenicolascv@gmail.com>
2025-11-16 17:30:13 -0300

    chore(contract): change update request

--- Features/Tags/Update/Contracts.cs
+++ Features/Tags/Update/Contracts.cs
@@ -1,5 +1,5 @@
 ﻿namespace SaldoFlex.API.Features.Tags.Update;
 
-public record UpdateTagRequest(Guid Id, string? Name);
+public record UpdateTagRequest(Guid Id, string Name);
 
 public record UpdateTagResponse(Guid Id, string Name);
