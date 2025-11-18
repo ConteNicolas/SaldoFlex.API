45137d50007ab9eac803cef6bcd859a357837b7b
Nicolas Conte <contenicolascv@gmail.com>
2025-11-17 21:32:25 -0300

    feat(domain): Add IsDefault property

--- Domain/Currency.cs
+++ Domain/Currency.cs
@@ -7,4 +7,5 @@ public class Currency : BaseEntity
     public string Description { get; set; }
     public string Code { get; set; }
     public string Symbol { get; set; }
+    public bool IsDefault { get; set; } = false;
 }
