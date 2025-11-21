d6bced202bc7cb69afafee91c4d172fdbed07a66
Nicolas Conte <contenicolascv@gmail.com>
2025-11-21 00:58:17 -0300

    feat(context): Add user and account configuration

---
+++ Infrastructure/Persistence/Configurations/AccountConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaldoFlex.API.Domain;
+
+namespace SaldoFlex.API.Infrastructure.Persistence.Configurations;
+
+public class AccountConfiguration : IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder.HasOne(x => x.User)
+            .WithOne(x => x.Account)
+            .HasForeignKey<Account>(x => x.UserId);
+    }
+}
---
+++ Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SaldoFlex.API.Domain;
+
+namespace SaldoFlex.API.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasOne(x => x.Account)
+            .WithOne(x => x.User)
+            .HasForeignKey<User>(x => x.AccountId);
+    }
+}
