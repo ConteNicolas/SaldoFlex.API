bc8da8e934d8df2bd5f18c6f177c1ddbea725a84
Nicolas Conte <contenicolascv@gmail.com>
2025-11-28 12:41:06 -0300

    ref: abstract configuration into a new class

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,64 @@
+﻿using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+using Microsoft.EntityFrameworkCore;
+using SaldoFlex.API.Infrastructure.Persistence;
+using SaldoFlex.API.Shared.Context;
+using SaldoFlex.API.Shared.Middlewares.Filters;
+using SaldoFlex.API.Shared.Services;
+
+namespace SaldoFlex.API.Infrastructure;
+
+public static class DependencyInjection
+{
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(opt =>
+        {
+            opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddFastEndpointWithJWTAndSwagger(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtCreationOptions>(opt =>
+        {
+            opt.SigningKey = configuration["JWT:Key"];
+            opt.Issuer = configuration["JWT:Issuer"];
+            opt.Audience = configuration["JWT:Audience"];
+        });
+
+        services
+            .AddAuthenticationJwtBearer(b => b.SigningKey = configuration["JWT:Key"])
+            .AddAuthorization()
+            .AddFastEndpoints()
+            .SwaggerDocument(opt =>
+            {
+                opt.DocumentSettings = b =>
+                {
+                    b.Title = "Saldo Flex";
+                    b.Description = "Saldo flex es una plataforma que te permite gestionar de forma eficiente tu dinero. El como lo seleccionas vos a traves de nuestro sistema flexible.";
+                    b.Version = "v1";
+                };
+            });
+
+        return services;
+    }
+
+    public static IServiceCollection AddLibraries(this IServiceCollection services)
+    {
+        services.AddMediatR(x => x.RegisterServicesFromAssemblies(typeof(Program).Assembly));
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<IResourceOwnershipService, ResourceOwnershipService>();
+        services.AddTransient<IUserContext, UserContext>();
+
+        return services;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,51 +1,30 @@
 global using FluentValidation;
 
 using FastEndpoints;
-using FastEndpoints.Security;
 using FastEndpoints.Swagger;
-using Microsoft.EntityFrameworkCore;
-using SaldoFlex.API.Infrastructure.Persistence;
-using SaldoFlex.API.Shared.Context;
+using SaldoFlex.API.Infrastructure;
 using SaldoFlex.API.Shared.Middlewares.Filters;
-using SaldoFlex.API.Shared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+builder.Services.AddCors(options =>
 {
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.Configure<JwtCreationOptions>(opt =>
-{
-    opt.SigningKey = builder.Configuration["JWT:Key"];
-    opt.Issuer = builder.Configuration["JWT:Issuer"];
-    opt.Audience = builder.Configuration["JWT:Audience"];
-});
-
-builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblies(typeof(Program).Assembly));
-
-builder.Services
-    .AddAuthenticationJwtBearer(b => b.SigningKey = builder.Configuration["JWT:Key"])
-    .AddAuthorization()
-    .AddFastEndpoints()
-    .SwaggerDocument(opt =>
+    options.AddDefaultPolicy(builder =>
     {
-        opt.DocumentSettings = b =>
-        {
-            b.Title = "Saldo Flex";
-            b.Description = "Saldo flex es una plataforma que te permite gestionar de forma eficiente tu dinero. El como lo seleccionas vos a traves de nuestro sistema flexible.";
-            b.Version = "v1";
-        };
+        builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
     });
+});
+
+builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddFastEndpointWithJWTAndSwagger(builder.Configuration);
+builder.Services.AddLibraries();
+builder.Services.AddServices();
 
 builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddTransient<IResourceOwnershipService, ResourceOwnershipService>();
-builder.Services.AddTransient<IUserContext, UserContext>();
-
 var app = builder.Build();
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
