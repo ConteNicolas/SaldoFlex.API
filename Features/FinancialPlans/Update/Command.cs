ca085f6e07c49f1cffa4b3a575890b74e7ac3b8c
Nicolas Conte <contenicolascv@gmail.com>
2025-12-01 21:49:52 -0300

    fix: Add logic for duplicate tag

--- Features/FinancialPlans/Update/Command.cs
+++ Features/FinancialPlans/Update/Command.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SaldoFlex.API.Domain;
 using SaldoFlex.API.Infrastructure.Persistence;
+using SaldoFlex.API.Shared.Context;
 using SaldoFlex.API.Shared.Models;
 
 namespace SaldoFlex.API.Features.FinancialPlans.Update;
@@ -15,23 +16,57 @@ public record UpdateFinancialPlanCommand(
 public class UpdateFinancialPlanCommandHandler : IRequestHandler<UpdateFinancialPlanCommand, Result<UpdateFinancialPlanResponse>>
 {
     private readonly ApplicationDbContext _context;
+    private readonly IUserContext _userContext;
 
-    public UpdateFinancialPlanCommandHandler(ApplicationDbContext context)
+    public UpdateFinancialPlanCommandHandler(ApplicationDbContext context, IUserContext userContext)
     {
         _context = context;
+        _userContext = userContext;
     }
 
     public async Task<Result<UpdateFinancialPlanResponse>> Handle(UpdateFinancialPlanCommand request, CancellationToken cancellationToken)
     {
-        var plan = await _context.FinancialPlans.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+        var plan = await _context.FinancialPlans
+            .Include(x => x.Tags)
+            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
 
         if (plan is null)
         {
             return Result.Failure<UpdateFinancialPlanResponse>(new Error("FinancialPlan.Update.NotFound", "Financial plan not found."));
         }
 
-        plan.Name = request?.Name ?? plan.Name;
-        plan.Description = request?.Description ?? plan.Description;
+        var userId = _userContext.GetUserId().GetValueOrDefault();
+
+        var existingDuplicateTag = plan.Tags.FirstOrDefault(x => x.Name.ToLower() == "duplicate");
+        if (existingDuplicateTag is not null && request?.Name != plan.Name)
+        {
+            plan.Tags.Remove(existingDuplicateTag);
+        }
+
+        var alreadyExistPlanWithSameName = await _context.FinancialPlans.AnyAsync(x => x.Name.ToLower() == request!.Name.ToLower() && x.Id != request.Id, cancellationToken);
+        if (alreadyExistPlanWithSameName && existingDuplicateTag is null)
+        {
+            var duplicateTag = await _context.Tags.FirstOrDefaultAsync(x => x.Name == "Duplicate" && x.UserId == userId, cancellationToken);
+
+            if (duplicateTag is null)
+            {
+
+                duplicateTag = new Tag()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Duplicate",
+                    CreatedAt = DateTime.UtcNow,
+                    UserId = userId
+                };
+
+                await _context.Tags.AddAsync(duplicateTag, cancellationToken);
+            }
+
+            plan.Tags.Add(duplicateTag);
+        }
+
+        plan.Name = request.Name;
+        plan.Description = request?.Description;
 
         plan.UpdatedAt = DateTime.UtcNow;
 
@@ -43,6 +78,12 @@ public class UpdateFinancialPlanCommandHandler : IRequestHandler<UpdateFinancial
 
     private UpdateFinancialPlanResponse MapToResponse(FinancialPlan plan)
     {
-        return new UpdateFinancialPlanResponse(plan.Id, plan.Name, plan?.Description);
+        var tags = MapTags(plan);
+        return new UpdateFinancialPlanResponse(plan.Id, plan.Name, plan?.Description, plan.CreatedAt, plan.UpdatedAt, tags);
+    }
+
+    private List<UpdateFinancialPlanTagResponse> MapTags(FinancialPlan plan)
+    {
+        return plan?.Tags.Select(x => new UpdateFinancialPlanTagResponse(x.Id, x.Name)).ToList() ?? new List<UpdateFinancialPlanTagResponse>();
     }
 }
